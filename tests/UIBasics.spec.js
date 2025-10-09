ddac444a5f605677117b0c3e2baa48100b7db40d
Subba <kvsubbareddy238@gmail.com>
2025-10-09 19:21:50 +0530

    Add static dropdown and window handles tests to UIBasics

--- tests/UIBasics.spec.js
+++ tests/UIBasics.spec.js
@@ -22,3 +22,68 @@ await userName.fill('rahulshettyacademy');
  console.log(allTitles);
 
     });
+
+    
+    test('static dropdown test',async ({page})=>{
+      await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
+    
+    const userName=page.locator("#username");
+    const passWord=page.locator("#password");
+    const dropdown=page.locator(".form-control[data-style='btn-info']")
+    await dropdown.selectOption("teach");
+    await expect(dropdown).toHaveValue("teach");
+    await page.locator(".radiotextsty").last().click();
+    await page.locator("#okayBtn").click();
+    console.log( await expect(page.locator(".radiotextsty").last()).toBeChecked());
+     await page.locator(".radiotextsty").last().isChecked();
+     await page.locator("#terms").click();
+    await expect(page.locator("#terms")).toBeChecked();
+ expect(await page.locator("#terms").isChecked());
+
+const blinkingText = page.locator(".blinkingText").first();
+
+// Get and log text
+const text = await blinkingText.textContent();
+console.log("Blinking text:", text);
+
+// Assert it has correct class attribute
+await expect(blinkingText).toHaveAttribute("class", "blinkingText");
+    });
+
+
+     test.only('window handles test',async ({page})=>{
+      await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
+    
+    const userName=page.locator("#username");
+    const passWord=page.locator("#password");
+    const dropdown=page.locator(".form-control[data-style='btn-info']")
+    await dropdown.selectOption("teach");
+    await expect(dropdown).toHaveValue("teach");
+    await page.locator(".radiotextsty").last().click();
+    await page.locator("#okayBtn").click();
+    console.log( await expect(page.locator(".radiotextsty").last()).toBeChecked());
+     await page.locator(".radiotextsty").last().isChecked();
+     await page.locator("#terms").click();
+    await expect(page.locator("#terms")).toBeChecked();
+ expect(await page.locator("#terms").isChecked());
+ 
+const blinkingText = page.locator(".blinkingText").first();
+
+// Get and log text
+const text = await blinkingText.textContent();
+console.log("Blinking text:", text);
+
+// Assert it has correct class attribute
+await expect(blinkingText).toHaveAttribute("class", "blinkingText");
+
+
+const [newPage] = await Promise.all([
+  page.waitForEvent('popup'), // wait for the new page event
+ blinkingText.click()//opens in new tab
+])
+await newPage.waitForLoadState('domcontentloaded');
+const title = await newPage.locator(".red").textContent();
+console.log("New page title:", title);
+
+     });
+
